5e090e6e0b333e5600d12c0ada7923e9e07ba2b4
Danil-Dol <dolbusin2005@gmail.com>
2025-10-08 08:59:32 +0500

    continue

--- shared/ui.js
+++ shared/ui.js
@@ -2,5 +2,11 @@
 
 
 export const Button = ({children}) =>{
-
+    return (
+        <div>
+            <a href="#">
+                <span>{children}</span>
+            </a>
+        </div>
+    )
 }
